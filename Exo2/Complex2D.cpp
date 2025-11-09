8a19af65e7ff4cf502d7ad680d02ecfc59a342b6
Bastien <94040097+JuiceKo@users.noreply.github.com>
2025-11-09 17:57:50 +0100

    Update Complex2D.cpp

--- Exo2/Complex2D.cpp
+++ Exo2/Complex2D.cpp
@@ -1,50 +1,56 @@
 #include "Complex2D.h"
+#include <iostream>
 #include <cmath>
+using namespace std;
 
+Complex2D::Complex2D() {
+    real = 0;
+    imag = 0;
+}
 
-Complex2D::Complex2D() { re = 0; im = 0; }
-Complex2D::Complex2D(double r, double i) { re = r; im = i; }
-Complex2D::Complex2D(const Complex2D& other) { re = other.re; im = other.im; }
+Complex2D::Complex2D(double r, double i) {
+    real = r;
+    imag = i;
+}
 
-double Complex2D::getRe() const { return re; }
-double Complex2D::getIm() const { return im; }
-void Complex2D::afficher() { cout << re << " + " << im << "i" << endl; }
-
-double Complex2D::module() const { return sqrt(re * re + im * im); }
+Complex2D::Complex2D(const Complex2D& other) {
+    real = other.real;
+    imag = other.imag;
+}
 
 Complex2D Complex2D::operator+(const Complex2D& other) const {
-    return Complex2D(re + other.re, im + other.im);
+    return Complex2D(real + other.real, imag + other.imag);
 }
 
 Complex2D Complex2D::operator-(const Complex2D& other) const {
-    return Complex2D(re - other.re, im - other.im);
+    return Complex2D(real - other.real, imag - other.imag);
 }
 
 Complex2D Complex2D::operator*(const Complex2D& other) const {
-    double r = re * other.re - im * other.im;
-    double i = re * other.im + im * other.re;
+    double r = real * other.real - imag * other.imag;
+    double i = real * other.imag + imag * other.real;
     return Complex2D(r, i);
 }
 
 Complex2D Complex2D::operator/(const Complex2D& other) const {
-    double denom = other.re * other.re + other.im * other.im;
-    double r = (re * other.re + im * other.im) / denom;
-    double i = (im * other.re - re * other.im) / denom;
+    double denom = other.real * other.real + other.imag * other.imag;
+    double r = (real * other.real + imag * other.imag) / denom;
+    double i = (imag * other.real - real * other.imag) / denom;
     return Complex2D(r, i);
 }
 
-bool Complex2D::operator==(const Complex2D& other) const {
-    return re == other.re && im == other.im;
-}
-
-bool Complex2D::operator!=(const Complex2D& other) const {
-    return !(*this == other);
-}
-
 bool Complex2D::operator<(const Complex2D& other) const {
-    return this->module() < other.module();
+    return module() < other.module();
 }
 
 bool Complex2D::operator>(const Complex2D& other) const {
-    return this->module() > other.module();
+    return module() > other.module();
+}
+
+void Complex2D::afficher() const {
+    cout << real << " + " << imag << "i" << endl;
+}
+
+double Complex2D::module() const {
+    return sqrt(real * real + imag * imag);
 }
