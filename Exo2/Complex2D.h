165a011190c49a22dfc8c45b939ebe14816bea44
Bastien <94040097+JuiceKo@users.noreply.github.com>
2025-11-09 17:57:27 +0100

    Update Complex2D.h

--- Exo2/Complex2D.h
+++ Exo2/Complex2D.h
@@ -1,31 +1,26 @@
 #ifndef COMPLEX2D_H
 #define COMPLEX2D_H
-#include <iostream>
-using namespace std;
 
 class Complex2D {
 private:
-    double re;
-    double im;
-
+    double real;
+    double imag;
+    
 public:
-    Complex2D();                            
-    Complex2D(double r, double i);
+    Complex2D();
+    Complex2D(double real, double imag);
     Complex2D(const Complex2D& other);
-
-    double getRe() const;
-    double getIm() const;
-    void afficher() const;
 
     Complex2D operator+(const Complex2D& other) const;
     Complex2D operator-(const Complex2D& other) const;
     Complex2D operator*(const Complex2D& other) const;
     Complex2D operator/(const Complex2D& other) const;
 
-    bool operator==(const Complex2D& other) const;
-    bool operator!=(const Complex2D& other) const;
     bool operator<(const Complex2D& other) const;
     bool operator>(const Complex2D& other) const;
+
+    void afficher() const;
+    double module() const;
 };
 
 #endif
